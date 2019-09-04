02bb30fbe9fd5608215afeeff0c76cf92ddd386c
noeljn <noel.johansson00@gmail.com>
2019-09-04 09:19:03 +0200

    Updated upp1 and upp2

---
+++ lab3/uppgift1.py
@@ -0,0 +1,8 @@
+def celToFah (c):
+    return round(((c*9)+160)/5,2)
+
+
+for c in range(0,21):
+    print(celToFah(c))
+
+
---
+++ lab3/uppgift2.py
@@ -0,0 +1,3 @@
+rader = input("Ange ett tal mellan 1-9")
+kolumner = input("Ange ett till tal mellan 1-9")
+
